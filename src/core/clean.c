04e94cd2b593425a1d11c5abe2f171aa951176ec
Mohammed Sajid Shaik <sajid.shaik1186@gmail.com>
2026-05-11 20:42:29 +0530

    feat: implement securezeromemory for windows

--- src/core/clean.c
+++ src/core/clean.c
@@ -2,12 +2,18 @@
 #include "butane.h"
 #include <string.h>
 
+#if defined(_WIN32)
+#include <windows.h>
+#endif
+
 // to: wipe memory without being optimized
 void butane_clean(void *ptr, size_t len) {
     if (!ptr || len == 0)
         return;
 #if defined(__STDC_LIB_EXT1__)
     memset_s(ptr, len, 0, len);
+#elif defined(_WIN32)
+    SecureZeroMemory(ptr, len);
 #else
     explicit_bzero(ptr, len);
 #endif
