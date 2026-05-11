602658df5c394a1860d3128f3addf1882e2efee1
Mohammed Sajid Shaik <sajid.shaik1186@gmail.com>
2026-05-11 20:44:59 +0530

    feat: implement msvc cpuid intrinsics

--- src/sys/hwdetect.c
+++ src/sys/hwdetect.c
@@ -1,13 +1,24 @@
 #include "hwdetect.h"
+
+#if defined(_MSC_VER)
+#include <intrin.h>
+#else
 #include <cpuid.h>
+#endif
 
 // check for hardware aes support
 int butane_cpu_has_aesni(void) {
+#if defined(_MSC_VER)
+    int cpuinfo[4];
+    __cpuid(cpuinfo, 1);
+    // aes-ni is bit 25 of ecx from cpuid leaf 1
+    return (cpuinfo[2] >> 25) & 1;
+#else
     unsigned int eax, ebx, ecx, edx;
 
     if (!__get_cpuid(1, &eax, &ebx, &ecx, &edx))
         return 0;
 
-    // aes-ni is bit 25 of ecx from cpuid leaf 1
     return (ecx >> 25) & 1;
+#endif
 }
